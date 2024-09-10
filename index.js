d9b7a6325fe2f19455a348eeab7b65bef3898327
BRYCEN-VN\dx_tri <dx_tri@brycen.com.vn>
2024-09-10 17:32:25 +0700

    update v1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-hello
+const express = require('express')
+var morgan = require('morgan')
+
+
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+    let a = 1;
+    let b = 2;
+    let c = a + b;
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
